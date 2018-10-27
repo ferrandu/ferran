c182c3bc4f5dd086c1f8147de2d1e86189bb6674
Ferran <ferran@mac.local>
2018-10-27 20:39:43 +0200

    Initial Commit

---
+++ ferran.api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using ferran.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ferran.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)  { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ ferran.api/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace ferran.api.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
